022cdb6e7635aeede448a97b6e8d471ce94cf386
malcolmbowers <malcolm.bowers@icloud.com>
2024-08-28 16:33:43 -0500

    Added the Array portion of the lab.

--- JavaScriptDemo/script.js
+++ JavaScriptDemo/script.js
@@ -1,11 +1,30 @@
+// 1. Declare a variable named `age` and assign it the value 25
 var age = 25;
+// 2. Declare a variable named `name` and assign it your name
 var name = 'Malcolm';
+// 3. Declare a variable named `isStudent` and assign it the value true
 var isStudent = true;
-
+// 4. Reassign the value of `age` to 30
 age = 30;
-
+// 5. Declare two variables `firstName` and `lastName`, and assign them
+// values. Then, create a third variable `fullName` that concatenates both.
 var firstName = 'Malcolm';
 var lastName = 'Bowers';
 var fullName = `${firstName} ${lastName}`;
-
 console.log(fullName);
+// 6. Create an array named `colors` with the values 'red', 'green', and 'blue'
+var colors = [
+    'red',
+    'green',
+    'blue'
+]
+// 7. Access the first element of the `colors` array and assign it to a 
+// variable `firstColor`
+firstColor = colors[0];
+console.log(firstColor);
+// 8. Change the second element of the `colors` array to 'yellow'
+colors[1] = 'yellow';
+// 9. Add 'purple' to the end of the `colors` array
+colors.push('purple');
+// 10. Find the length of the `colors` array and assign it to a variable `colorsLength`
+var colorsLength = colors.length;
