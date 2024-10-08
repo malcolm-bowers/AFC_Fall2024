6b7d4c113995b2d1608240a89188225c492c4afe
malcolmbowers <malcolm.bowers@icloud.com>
2024-10-08 09:10:48 -0500

    initial form demo commit

---
+++ REACT/formdemo3/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <form action="">
+        <label htmlFor='fname'>First Name:
+          <input type='text' name='fname' id='fname' />
+        </label>
+      </form>
+    </>
+  )
+}
+
+export default App
